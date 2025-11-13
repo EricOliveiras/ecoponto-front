da9a968d8ab930295355019e56970c93ba3e4572
Eric Oliveira <oiericoi@hotmail.com>
2025-11-13 10:36:40 -0300

    Refactor: update main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// 1. CSS do Tailwind
+import "./index.css";
+
+// 2. CSS do Mapa (Mapbox GL)
+import "mapbox-gl/dist/mapbox-gl.css";
+
+// --- ATUALIZAÇÃO AQUI ---
+
+// 3. REMOVA O CSS ANTIGO (do search-js-react)
+// import "@mapbox/search-js-react/dist/search-js-react.css";
+
+// 4. ADICIONE O CSS NOVO (do mapbox-gl-geocoder)
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+// ------------------------
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
